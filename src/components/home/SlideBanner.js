c47c29ac467df3270dbbb65856d54f6edc85cd19
Mangor1no <39774481+Mangor1no@users.noreply.github.com>
2022-01-06 04:55:29 +0700

    final

--- src/components/home/SlideBanner.js
+++ src/components/home/SlideBanner.js
@@ -1,13 +1,73 @@
 import * as React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Link from 'next/link';
 
-export const SlideBanner = ({ banner }) => {
-  return (
-    <div className="w-full h-full relative" key={banner.id}>
-      <img
-        src={banner.image}
-        alt="banner"
-        className="w-auto h-full object-cover z-0 max-h-[760px] min-h-[760px]"
-      />
-    </div>
-  );
+const carouselSetting = {
+  autoPlay: true,
+  infiniteLoop: true,
+  showArrows: false,
+  showThumbs: false,
+  showStatus: false,
+  interval: 6000,
+  showIndicators: false,
 };
+
+export const SlideBanner = ({ banners }) => (
+  <Carousel {...carouselSetting} style={{ height: '100%', minHeight: 'inherit' }}>
+    {banners.map((banner) => (
+      <div
+        className="w-full h-full min-h-[600px] lg:min-h-[900px] flex text-[#F2F2F2] z-10"
+        style={{ backgroundColor: banner.background }}
+        key={banner.id}
+      >
+        <div
+          className="flex items-center px-4 sm:px-20 xl:px-40 2xl:px-60 3xl:px-[368px] relative w-full h-full"
+          style={{ minHeight: 'inherit' }}
+        >
+          <div
+            className={`${banner.id > 0 ? 'items-end' : 'items-start'} flex flex-col w-full z-10`}
+          >
+            <div className="flex flex-col items-start">
+              {banner.id < 1 && (
+                <p className="text-base md:text-lg font-bold inline-flex items-center space-x-3 mb-4">
+                  <div className="h-[3px] w-14 bg-yellow-100" />
+                  <span className="text-[#F2F2F2]">UP TO 40% OFF</span>
+                </p>
+              )}
+              <p
+                className={`${
+                  banner.id > 0 ? 'text-[#000000]' : 'text-[#F2F2F2]'
+                } capitalize text-4xl lg:text-6xl xl:text-7xl xl:leading-[102px] font-bold mb-4 whitespace-pre-line text-left`}
+              >
+                {banner.title}
+              </p>
+              {banner?.description && (
+                <p className="mb-[50px] text-black-100 font-neue max-w-[470px]">
+                  {banner?.description}
+                </p>
+              )}
+              <Link href="/shop/food">
+                <button
+                  type="button"
+                  className={`${
+                    banner.id > 0 ? 'btn-secondary' : 'btn-primary'
+                  } btn-big text-bold uppercase transition duration-150`}
+                >
+                  Shop now
+                </button>
+              </Link>
+            </div>
+          </div>
+          <img
+            src={banner.image}
+            alt="banner"
+            className={`hidden md:block absolute bottom-0 w-2/3 ${
+              banner.id > 0 ? 'left-0 max-w-[560px]' : 'right-0 max-w-[960px]'
+            }`}
+          />
+        </div>
+      </div>
+    ))}
+  </Carousel>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,39 @@
-import HomeCategory from 'components/home/HomeCategory';
-import Layout from 'components/Layout';
+import HomeBestDeal from 'components/home/HomeBestDeal';
+import HomeBestProduct from 'components/home/HomeBestProduct';
 import HomeBestSeller from 'components/home/HomeBestSeller';
 import HomeBlog from 'components/home/HomeBlog';
-import Link from 'next/link';
-import HomeSale from 'components/home/HomeSale';
-import HomeBestDeal from 'components/home/HomeBestDeal';
+import HomeCategory from 'components/home/HomeCategory';
 import HomeFlashDeal from 'components/home/HomeFlashDeal';
-import HomeBestProduct from 'components/home/HomeBestProduct';
+import HomeSale from 'components/home/HomeSale';
+import { SlideBanner } from 'components/home/SlideBanner';
+import Layout from 'components/Layout';
+import banner01 from 'public/images/home/banner.png';
+import banner02 from 'public/images/home/banner-2.png';
+
+const images = [banner01, banner02];
+
+const banners = [
+  {
+    id: 0,
+    image: banner01,
+    title: 'A Greate Meal \n With Your Pet',
+    background: '#45A7DE',
+  },
+  {
+    id: 1,
+    image: banner02,
+    title: 'Best food \n for your pet',
+    background: '#FFD868',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non felis, id amet risus pellentesque fermentum. Augue ultricies.',
+  },
+];
 
 export default function Home() {
   return (
     <Layout>
-      <div className="w-full h-full min-h-[600px] lg:min-h-[900px] bg-blue-100 flex items-center justify-center text-[#F2F2F2] z-10">
-        <div
-          className="flex items-center px-4 sm:px-20 xl:px-40 2xl:px-60 3xl:px-[368px] relative w-full h-full"
-          style={{ minHeight: 'inherit' }}
-        >
-          <div className="w-full md:w-1/2 z-10">
-            <p className="text-base md:text-lg font-bold inline-flex items-center space-x-3 mb-4">
-              <div className="h-[3px] w-14 bg-yellow-100" />
-              <span>UP TO 40% OFF</span>
-            </p>
-            <p className="capitalize text-4xl lg:text-6xl xl:text-7xl xl:leading-[102px] font-bold mb-4">
-              A Greate Meal <br /> With Your Pet
-            </p>
-            <Link href="/shop/food">
-              <button
-                type="button"
-                className="btn-primary btn-big text-bold uppercase transition duration-150"
-              >
-                Shop now
-              </button>
-            </Link>
-          </div>
-          <img
-            src="images/home/banner.png"
-            alt="banner"
-            className="hidden md:block absolute bottom-0 right-0 w-2/3 max-w-[960px]"
-          />
-        </div>
+      <div className="w-full h-full relative min-h-[600px] lg:min-h-[900px] bg-blue-100 flex items-center justify-center text-[#F2F2F2] z-10">
+        <SlideBanner banners={banners} images={images} />
       </div>
       <div className="px-5 sm:px-16 xl:px-[150px] 2xl:px-[370px] w-full py-16 md:pt-[50px] md:pb-[86px] bg-black-2">
         <HomeCategory />
